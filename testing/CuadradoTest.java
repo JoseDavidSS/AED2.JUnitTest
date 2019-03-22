09815985c8092a0e3a623938a0dcc40c82007b41
Alejandro <josealejandroibarra@hotmail.com>
2019-03-22 13:16:35 -0600

    Agregados comentarios.

--- testing/CuadradoTest.java
+++ testing/CuadradoTest.java
@@ -32,5 +32,19 @@ public class CuadradoTest {
         server.connect(c);
     }
 
+    // Es una forma de comprobar el correcto funcionamiento de una parte del código
+    // en especifico, esto con el fin de asegurar que cada unidad sea eficiente por separado.
+    // La idea central es escribir casos de prueba para cada método de forma independiente,
+    // básicamente ignorando el resto del código. Posteriormente las partes independientes probadas
+    // se integran para un funcionamiento completo.
+
+    // JUnit es un conjunto de bibliotecas utilizadas en Java para facilitar la implementación
+    // de pruebas unitarias y al mismo tiempo codificar el proyecto principal.
+
+    // JMockit y Mockito son marcos de prueba que permiten crear Mock-Object (Objetos de burla),
+    // los cuales son substitutos de objetos reales para que se utilicen como temporales si es necesario.
+    // Un ejemplo seria si la complejidad del objeto real es alta y se necesita obtener datos específicos
+    // como respuesta.
+
 
 }
